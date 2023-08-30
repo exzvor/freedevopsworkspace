ddc30c4ac50476459de1ea58c2319f3f4f0bfee8
victor <victorbejenar@yandex.ru>
2023-08-30 23:21:06 +0300

    cs edit practical take refresh

--- CS/task/main.go
+++ CS/task/main.go
@@ -1,53 +1,63 @@
+//Утилита считывает содержимое каждого указанного каталога и отображает информацию о каждом файле внутри.
+//Эта информация включает в себя имя файла, его размер и мод доступа.
+
 package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strconv"
 )
 
 // Функция для считывания директории и вывода файлов внутри неё
-func ReadDir(directory string) error {
-	fileInfos, err := ioutil.ReadDir(directory)
+func readDir(directory string) error {
+	fileInfos, err := os.ReadDir(directory)
 	if err != nil {
-		fmt.Printf("Не удалось открыть директорию %s: %v\n", directory, err)
+		fmt.Printf("Failed to open directory %s: %v\n\n", directory, err)
 		return err
 	}
 
 	for _, fileInfo := range fileInfos {
-		fileInfoStr := FormatFileInfo(directory, fileInfo)
+		fileInfoStr := formatFileInfo(fileInfo)
 		fmt.Println(fileInfoStr)
 	}
 	return nil
 }
 
 // Функция для форматирования информации о файле
-func FormatFileInfo(directory string, fileInfo os.FileInfo) string {
-	fileSize := strconv.FormatInt(fileInfo.Size(), 10)
-	fileMode := fileInfo.Mode().String()
-	fileName := fileInfo.Name()
+func formatFileInfo(fileInfo os.DirEntry) string {
+	info, err := fileInfo.Info()
+	if err != nil {
+		fmt.Println("Error occurred while obtaining file information: ", err)
+		return "exit"
+	}
 
-	return fmt.Sprintf("%s %s %s", fileMode, fileSize, fileName)
+	fileSize := strconv.FormatInt(info.Size(), 10)
+	fileMode := info.Mode().String()
+	fileName := info.Name()
+
+	return fmt.Sprintf("%s %s %s", fileName, fileSize, fileMode)
 }
 
 // Основная функция, принимающая аргументы командной строки
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Необходимо предоставить аргументы (папки).")
+		fmt.Println("It is necessary to specify directory names")
 		return
 	}
 
 	for _, dir := range os.Args[1:] {
 		absDir, err := filepath.Abs(dir)
 		if err != nil {
-			fmt.Printf("Не удалось получить абсолютный путь для %s: %v\n", dir, err)
+			fmt.Printf("Failed to get an absolute path for %s: %v\n", dir, err)
 			continue
 		}
-		ReadDir(absDir)
+
+		err = readDir(absDir)
+		if err != nil {
+			fmt.Printf("Failed to read directory %s: %v\n", absDir, err)
+			continue
+		}
 	}
 }
-
-//Утилита myls считывает содержимое каждого указанного каталога и отображает информацию о каждом файле внутри.
-//Эта информация включает в себя мод доступа к файлу, его размер и имя.
